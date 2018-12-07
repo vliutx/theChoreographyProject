814c31ff499c32ff5c108e7eb47584f63a355ef8
Vincent Liu <43043384+vincentpoops@users.noreply.github.com>
2018-12-07 13:04:47 -0600

    started phase 6

---
+++ Phase VI/item.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>The Choreography Project</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom fonts for this template -->
+    <link href="css/all.min.css" rel="stylesheet">
+    <link href="css/simple-line-icons.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+    <!-- Custom styles for this template -->
+    <link href="css/landing-page.min.css" rel="stylesheet">
+    <!-- Bootstrap core JavaScript -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+  </head>
+
+  <body>
+    <!-- Navigation -->
+    <nav class="navbar navbar-light bg-light static-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">Home</a>
+        <a class="navbar-brand" href="about.html">About</a>
+        <a class="navbar-brand" href="contact.html">Contact</a>
+        <a class="btn btn-secondary" href="register.html">Register</a>
+        <a class="btn btn-primary" href="login.html">Sign In</a>
+      </div>
+    </nav>
+
+    <!-- Image Showcases -->
+    <section class="showcase">
+      <div class="container-fluid p-0">
+        <div class="row no-gutters">
+          <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('https://www.youtube.com/watch?v=HVR3h12LOkA');"></div>
+          <div class="col-lg-6 order-lg-1 my-auto showcase-text">
+            <h2>Community</h2>
+            <p class="lead mb-0">As a community of dancers, we are constantly growing and seeking inspiration from others. Our choreography library offers a resource that satisfies this need, as well as creates an online community through which dancers can interact with and learn with.</p>
+          </div>
+          
+        </div>
+        <div class="row no-gutters">
+          <div class="col-lg-6 text-white showcase-img" style="background-image: url('img/bg-showcase-2.jpg');"></div>
+          <div class="col-lg-6 my-auto showcase-text">
+            <h2>Share &amp; Search for Videos</h2>
+            <p class="lead mb-0">Trying to find inspiration? Look no further! Iajsiofjaiosfjiaosfjioasjfioajsfo this is just filler text and I don't know what we're doing.</p>
+          </div>
+        </div>
+      </div>
+    </section>
---
+++ Phase VI/js/register.js
@@ -0,0 +1,132 @@
+document.getElementById("credentials").submit.onclick = validate;
+
+function validate () {
+	var userVal = document.getElementById("user").value;
+	var pass = document.getElementById("pass").value;
+	var pass2 = document.getElementById("pass2").value;
+	var userValid = checkUser(userVal);
+	var passValid = checkPass(pass, pass2);
+	if (!userValid || !passValid){
+		window.location.href="register.php?invalid=1";
+	}
+	else {
+		window.location.href="register.php?invalid=0";
+	}
+}
+
+function checkUser(userVal) {
+	
+	// acceptable characters
+	var alphaL = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+	var alphaU = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+	var num = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+	
+	// check user name length
+	console.log(userVal.length);
+	if (userVal.length < 6 || userVal.length > 10) {
+		return false;
+	}
+	
+	// check if first character is a digit
+	for (i=0; i < num.length; i++) {
+		if (userVal.charAt(0) == num[i]) {
+			return false;
+		}
+	}
+	
+	// check if any non-letters or non-digits are used
+	// loop through each character of user name
+	for (i=0; i < userVal.length; i++) {	
+		var isLetter = false;
+		var isNum = false;
+		
+		// confirm that character is a letter
+		for (j=0; j < alphaL.length; j++) {
+			if (userVal.charAt(i) == alphaL[j] || userVal.charAt(i) == alphaU[j]) {
+				isLetter = true;
+			}
+		}
+		
+		// confirm that character is a number
+		for (j=0; j < num.length; j++) {
+			if (userVal.charAt(i) == num[j]) {
+				isNum = true;
+			}
+		}
+		
+		// return false if character is neither a letter nor number
+		// if character is a letter or number, continue looping through user name string
+		if (!isLetter && !isNum) {
+			return false;
+		}
+	}
+	
+	return true;
+}
+
+function checkPass(pass, pass2) {
+	
+	// acceptable characters
+	var alphaL = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+	var alphaU = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+	var num = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+	
+	// check if pass1 and pass2 match
+	if (pass != pass2) {
+		return false;
+	}
+	
+	console.log(pass.length);
+	if (pass.length < 6 || pass.length > 10) {
+		return false;
+	}
+	
+	// check if any non-letters or non-digits are used
+	// also check if at least one lowercase letter, one uppercase letter, and one digit are used
+	// loop through each character of user name
+	var hasLower = 0;
+	var hasUpper = 0;
+	var hasDigit = 0;
+	
+	for (i=0; i < pass.length; i++) {	
+		var isLetter = false;
+		var isNum = false;
+		
+		// confirm that character is a lowercase letter
+		for (j=0; j < alphaL.length; j++) {
+			if (pass.charAt(i) == alphaL[j]) {
+				isLetter = true;
+				hasLower++;
+			}
+		}
+		
+		// confirm that character is an uppercase letter
+		for (j=0; j < alphaU.length; j++) {
+			if (pass.charAt(i) == alphaU[j]) {
+				isLetter = true;
+				hasUpper++;
+			}
+		}
+		
+		// confirm that character is a number
+		for (j=0; j < num.length; j++) {
+			if (pass.charAt(i) == num[j]) {
+				isNum = true;
+				hasDigit++;
+			}
+		}
+		
+		// return false if character is neither a letter nor number
+		// if character is a letter or number, continue looping through user name string
+		if (!isLetter && !isNum) {
+			return false;
+		}
+	}
+	
+	// return false if missing a lowercase letter, uppercase letter, or digit
+	if (hasUpper == 0 || hasLower == 0 || hasDigit == 0) {
+		return false;
+	}
+	
+	return true;
+}
